ddd6d2687049ade62f449071dc6ab9897c6d21e0
Vinge Nikolay <vn2756@gmail.com>
2022-04-28 20:59:46 +0400

    InjectModule

--- Luna.IDE/Infrastructure/DependencyContainer.cs
+++ Luna.IDE/Infrastructure/DependencyContainer.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using Luna.IDE.Commands;
-using Luna.IDE.Model;
-using Luna.IDE.ViewModel;
-using Luna.Infrastructure;
 
 namespace Luna.IDE.Infrastructure
 {
@@ -13,28 +9,7 @@ namespace Luna.IDE.Infrastructure
         static DependencyContainer()
         {
             _container = DependencyInjection.DependencyContainerFactory.MakeLiteContainer();
-            InitContainer();
-        }
-
-        private static void InitContainer()
-        {
-            _container.Bind<IFileSystem, FileSystem>().ToSingleton();
-            _container.Bind<IOpenFileDialog, OpenFileDialog>();
-
-            _container.Bind<IProjectItemEditorFactory, ProjectItemEditorFactory>().ToSingleton();
-
-            _container.Bind<IEnvironmentWindowsManager, EnvironmentWindowsManager>().ToSingleton();
-            _container.Bind<EnvironmentWindowsViewModel, EnvironmentWindowsViewModel>().ToSingleton();
-
-            _container.Bind<IProjectExplorer, ProjectExplorer>().ToSingleton();
-            _container.Bind<ProjectExplorerViewModel, ProjectExplorerViewModel>().ToSingleton();
-            _container.Bind<ProjectTreeViewModel, ProjectTreeViewModel>().ToSingleton();
-
-            _container.Bind<IOpenProjectCommand, OpenProjectCommand>().ToSingleton();
-            _container.Bind<IProjectItemOpenCommand, ProjectItemOpenCommand>().ToSingleton();
-            _container.Bind<IProjectTreeItemOpenCommand, ProjectTreeItemOpenCommand>().ToSingleton();
-
-            _container.Bind<MainViewModel, MainViewModel>().ToSingleton();
+            _container.InitFromModules(new InjectModule());
         }
 
         public static TDependency Resolve<TDependency>()
---
+++ Luna.IDE/InjectModule.cs
@@ -0,0 +1,33 @@
+﻿using DependencyInjection;
+using Luna.IDE.Commands;
+using Luna.IDE.Infrastructure;
+using Luna.IDE.Model;
+using Luna.IDE.ViewModel;
+using Luna.Infrastructure;
+
+namespace Luna.IDE
+{
+    public class InjectModule : DependencyInjection.InjectModule
+    {
+        public override void Init(IBindingProvider provider)
+        {
+            provider.Bind<IFileSystem, FileSystem>().ToSingleton();
+            provider.Bind<IOpenFileDialog, OpenFileDialog>();
+
+            provider.Bind<IProjectItemEditorFactory, ProjectItemEditorFactory>().ToSingleton();
+
+            provider.Bind<IEnvironmentWindowsManager, EnvironmentWindowsManager>().ToSingleton();
+            provider.Bind<EnvironmentWindowsViewModel, EnvironmentWindowsViewModel>().ToSingleton();
+
+            provider.Bind<IProjectExplorer, ProjectExplorer>().ToSingleton();
+            provider.Bind<ProjectExplorerViewModel, ProjectExplorerViewModel>().ToSingleton();
+            provider.Bind<ProjectTreeViewModel, ProjectTreeViewModel>().ToSingleton();
+
+            provider.Bind<MainViewModel, MainViewModel>().ToSingleton();
+
+            provider.Bind<IOpenProjectCommand, OpenProjectCommand>().ToSingleton();
+            provider.Bind<IProjectItemOpenCommand, ProjectItemOpenCommand>().ToSingleton();
+            provider.Bind<IProjectTreeItemOpenCommand, ProjectTreeItemOpenCommand>().ToSingleton();
+        }
+    }
+}

014f8a0ebb079e2a8e999317b85a080cc995da7b
Vinge Nikolay <vn2756@gmail.com>
2022-08-15 19:20:19 +0400

    ProjectItemEditorFactory refactoring

--- Luna.IDE/Model/ProjectItemEditorFactory.cs
+++ Luna.IDE/Model/ProjectItemEditorFactory.cs
@@ -28,13 +28,20 @@ public class ProjectItemEditorFactory : IProjectItemEditorFactory
     public EnvironmentWindowComponents MakeEditorFor(ProjectItem projectItem)
     {
         var types = Assembly.GetExecutingAssembly().GetTypes();
+        var model = MakeModel(types, projectItem);
+        var viewModel = MakeViewModel(types, projectItem, model);
+        var view = MakeView(types, projectItem, viewModel);
 
-        var modelType = types
-            .FirstOrDefault(type =>
-                type.GetCustomAttribute<EditorForAttribute>()?.ProjectItemType == projectItem.GetType() &&
-                type.GetConstructors().Length == 1 &&
-                type.GetConstructors().First().GetParameters().Length > 0 &&
-                type.GetConstructors().First().GetParameters().First().ParameterType == projectItem.GetType()) ?? throw new ProjectItemEditorFactoryException();
+        return new EnvironmentWindowComponents(model, view);
+    }
+
+    private IEnvironmentWindowModel MakeModel(IEnumerable<Type> types, ProjectItem projectItem)
+    {
+        var modelType = types.FirstOrDefault(type =>
+            type.GetCustomAttribute<EditorForAttribute>()?.ProjectItemType == projectItem.GetType() &&
+            type.GetConstructors().Length == 1 &&
+            type.GetConstructors().First().GetParameters().Length > 0 &&
+            type.GetConstructors().First().GetParameters().First().ParameterType == projectItem.GetType()) ?? throw new ProjectItemEditorFactoryException();
 
         var modelTypeConstructorParams = new List<object> { projectItem };
         foreach (var parameter in modelType.GetConstructors().First().GetParameters().Skip(1))
@@ -45,12 +52,16 @@ public class ProjectItemEditorFactory : IProjectItemEditorFactory
         var model = Activator.CreateInstance(modelType, modelTypeConstructorParams.ToArray()) as IEnvironmentWindowModel ?? throw new ProjectItemEditorFactoryException();
         DependencyContainer.ResolveInjectedProperties(model);
 
-        var viewModelType = types
-            .FirstOrDefault(type =>
-                type.GetCustomAttribute<EditorForAttribute>()?.ProjectItemType == projectItem.GetType() &&
-                type.GetConstructors().Length == 1 &&
-                type.GetConstructors().First().GetParameters().Length > 0 &&
-                type.GetConstructors().First().GetParameters().First().ParameterType.IsInstanceOfType(model)) ?? throw new ProjectItemEditorFactoryException();
+        return model;
+    }
+
+    private object MakeViewModel(IEnumerable<Type> types, ProjectItem projectItem, IEnvironmentWindowModel model)
+    {
+        var viewModelType = types.FirstOrDefault(type =>
+            type.GetCustomAttribute<EditorForAttribute>()?.ProjectItemType == projectItem.GetType() &&
+            type.GetConstructors().Length == 1 &&
+            type.GetConstructors().First().GetParameters().Length > 0 &&
+            type.GetConstructors().First().GetParameters().First().ParameterType.IsInstanceOfType(model)) ?? throw new ProjectItemEditorFactoryException();
 
         var viewModelTypeConstructorParams = new List<object> { model };
         foreach (var parameter in viewModelType.GetConstructors().First().GetParameters().Skip(1))
@@ -61,14 +72,18 @@ public class ProjectItemEditorFactory : IProjectItemEditorFactory
         var viewModel = Activator.CreateInstance(viewModelType, viewModelTypeConstructorParams.ToArray()) ?? throw new ProjectItemEditorFactoryException();
         DependencyContainer.ResolveInjectedProperties(viewModel);
 
-        var viewType = types
-            .FirstOrDefault(type =>
-                type.GetCustomAttribute<EditorForAttribute>()?.ProjectItemType == projectItem.GetType() &&
-                type.GetConstructor(new[] { viewModel.GetType() }) != null) ?? throw new ProjectItemEditorFactoryException();
+        return viewModel;
+    }
+
+    private object MakeView(IEnumerable<Type> types, ProjectItem projectItem, object viewModel)
+    {
+        var viewType = types.FirstOrDefault(type =>
+            type.GetCustomAttribute<EditorForAttribute>()?.ProjectItemType == projectItem.GetType() &&
+            type.GetConstructor(new[] { viewModel.GetType() }) != null) ?? throw new ProjectItemEditorFactoryException();
 
         var view = Activator.CreateInstance(viewType, viewModel) ?? throw new ProjectItemEditorFactoryException();
 
-        return new EnvironmentWindowComponents(model, view);
+        return view;
     }
 }
 

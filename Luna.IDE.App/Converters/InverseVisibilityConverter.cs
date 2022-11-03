00b17ae5aeed04f773e8cd3922b570a8ae5bf3fd
Vinge Nikolay <vn2756@gmail.com>
2022-11-03 20:29:15 +0400

    InverseVisibilityConverter, InverseBooleanToVisibilityConverter

--- Luna.IDE.App/Converters/InverseVisibilityConverter.cs
+++ Luna.IDE.App/Converters/InverseVisibilityConverter.cs
@@ -4,11 +4,12 @@ using System.Windows.Data;
 
 namespace Luna.IDE.App.Converters;
 
-public class InverseBooleanToVisibilityConverter : IValueConverter
+public class InverseVisibilityConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return ((bool)value) ? Visibility.Collapsed : Visibility.Visible;
+        var visibility = (Visibility)value;
+        return visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
